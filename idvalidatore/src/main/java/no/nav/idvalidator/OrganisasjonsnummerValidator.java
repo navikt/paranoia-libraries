39886eecfb647cc2cd0481aa4e4da97b006d76d1
H141598 <bernt.halseide@nav.no>
2019-03-12 15:53:15 +0100

    implementertasjonen av testen (TDD)

--- idvalidatore/src/main/java/no/nav/idvalidator/OrganisasjonsnummerValidator.java
+++ idvalidatore/src/main/java/no/nav/idvalidator/OrganisasjonsnummerValidator.java
@@ -1,11 +1,20 @@
 package no.nav.idvalidator;
 
+import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.IntStream;
 
 public class OrganisasjonsnummerValidator implements Predicate<String> {
 
     @Override
-    public boolean test(String s) {
-        return false;
+    public boolean test(String orgnr) {
+        if(orgnr == null ||!orgnr.matches("\\d{9}") ) return false;
+        List<Integer> vekttall = List.of(3,2,7,6,5,4,3,2);
+        int sum = IntStream.range(0,orgnr.length()-1)
+                .map(i -> (Integer.parseInt(orgnr.substring(i, i + 1)) * vekttall.get(i)))
+                .sum();
+        int rest = sum % 11;
+        if(rest== 0 && Integer.parseInt(orgnr.substring(orgnr.length()-1)) == rest) return true;
+        return Integer.parseInt(orgnr.substring(orgnr.length()-1))== 11 - rest;
     }
 }
